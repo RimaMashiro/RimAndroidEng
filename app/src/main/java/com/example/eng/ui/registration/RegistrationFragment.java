bc5139d6f7095befef9550609d952df885c0fcbd
Mashiro Rima <vladlenakarimova1@gmail.com>
2023-04-10 19:48:03 +0500

    First

---
+++ app/src/main/java/com/example/eng/ui/registration/RegistrationFragment.java
@@ -0,0 +1,118 @@
+package com.example.eng.ui.registration;
+
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.example.eng.R;
+import com.example.eng.databinding.FragmentRegistrationBinding;
+
+
+public class RegistrationFragment extends Fragment {
+
+    private RegistrationViewModel viewModel;
+    private FragmentRegistrationBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ViewModelProvider provider = new ViewModelProvider(requireActivity());
+        viewModel = provider.get(RegistrationViewModel.class);
+        initEditTextEmail();
+        initEditTextPassword();
+        initButtonRegister();
+        initButtonGoToSign();
+        initNavigationToSignFragment();
+    }
+
+    private void initEditTextEmail() {
+
+        binding.editTextEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                viewModel.onEmailChanged(editable.toString());
+            }
+        });
+
+
+    }
+
+    private void initEditTextPassword() {
+
+        binding.editTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                viewModel.onPasswordChanged(editable.toString());
+            }
+        });
+    }
+
+    private void initButtonRegister() {
+
+        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.onButtonRegisterClicked();
+            }
+        });
+    }
+
+    private void initButtonGoToSign() {
+
+        binding.buttonGoToSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.onButtonGoToSignClicked();
+            }
+        });
+    }
+
+    private void initNavigationToSignFragment() {
+        viewModel.navigationToSignFragment.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    Navigation.findNavController(binding.getRoot()).navigate(R.id.action_registrationFragment_to_signFragment);
+
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/eng/ui/registration/RegistrationViewModel.java
@@ -0,0 +1,29 @@
+package com.example.eng.ui.registration;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RegistrationViewModel extends ViewModel {
+    private String email;
+    private  String password;
+
+    private MutableLiveData<Boolean> _navigationToSignFragment= new MutableLiveData<Boolean>(false) ;
+    LiveData<Boolean> navigationToSignFragment= _navigationToSignFragment ;
+    public void onEmailChanged(String email){
+        this.email=email;
+    }
+
+    public void onPasswordChanged(String password){
+        this.password=password;
+    }
+
+    public void onButtonRegisterClicked(){
+        _navigationToSignFragment.setValue(true);
+    }
+
+    public void onButtonGoToSignClicked(){
+
+    }
+
+}
