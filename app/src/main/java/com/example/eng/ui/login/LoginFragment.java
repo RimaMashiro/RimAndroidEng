64a50c7250d542c6d10da9549b4d0d3e98cfa079
Mashiro Rima <vladlenakarimova1@gmail.com>
2023-04-10 20:19:45 +0500

    LoginFragment

---
+++ app/src/main/java/com/example/eng/ui/login/LoginFragment.java
@@ -0,0 +1,120 @@
+package com.example.eng.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eng.R;
+import com.example.eng.databinding.FragmentLoginBinding;
+
+
+public class LoginFragment extends Fragment {
+
+
+    private LoginViewModel viewModel;
+
+    private FragmentLoginBinding binding;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ViewModelProvider provider= new ViewModelProvider(requireActivity());
+        viewModel= provider.get(LoginViewModel.class);
+        initEditTextSignEmail();
+        initEditTextSignPassword();
+        initButtonSign();
+        initButtonPasswordReset();
+    }
+
+    private void initEditTextSignEmail(){
+
+        binding.editTextSignEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                viewModel.onEmailChanged(editable.toString());
+            }
+        });
+
+
+    }
+    private void initEditTextSignPassword(){
+
+        binding.editTextSignPassword.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                viewModel.onPasswordChanged(editable.toString());
+            }
+        });
+    }
+
+    private void initButtonSign(){
+
+        binding.buttonSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.onButtonSignClicked();
+            }
+        });
+    }
+
+    private void initButtonPasswordReset() {
+        binding.buttonPasswordReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.onButtonPasswordResetClicked();
+            }
+        });
+    }
+
+    private void initNavigationToPasswordResetFragment() {
+        viewModel.navigationToPasswordResetFragment.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    Navigation.findNavController(binding.getRoot()).navigate(R.id.action_signFragment_to_passwordResetFragment);
+
+                }
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/eng/ui/login/LoginViewModel.java
@@ -0,0 +1,40 @@
+package com.example.eng.ui.login;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import androidx.lifecycle.ViewModel;
+
+import android.util.Patterns;
+/*
+import com.example.myapplication1.data.LoginRepository;
+import com.example.myapplication1.data.Result;
+import com.example.myapplication1.data.model.LoggedInUser;
+
+ */
+import com.example.eng.R;
+
+public class LoginViewModel extends ViewModel {
+
+    private String email;
+    private  String password;
+
+    private MutableLiveData<Boolean> _navigationToPasswordResetFragment= new MutableLiveData<Boolean>(false) ;
+
+    LiveData<Boolean> navigationToPasswordResetFragment= _navigationToPasswordResetFragment;
+    public void onEmailChanged(String email){
+        this.email=email;
+    }
+
+    public void onPasswordChanged(String password){
+        this.password=password;
+    }
+
+    public void onButtonSignClicked(){
+
+    }
+
+    public void onButtonPasswordResetClicked(){
+        _navigationToPasswordResetFragment.setValue(true);
+    }
+}
